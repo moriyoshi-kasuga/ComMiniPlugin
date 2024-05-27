9687276658e4a98ec365eb444e50444dbcd86ab3
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-05-27 18:35:34 +0900

    feat(fastboard): add manager class :sparkles:

---
+++ src/main/java/github/moriyoshi/comminiplugin/dependencies/fastboard/FastBoardManager.java
@@ -0,0 +1,54 @@
+package github.moriyoshi.comminiplugin.dependencies.fastboard;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import org.bukkit.entity.Player;
+
+import lombok.val;
+
+public class FastBoardManager {
+
+  private final HashMap<UUID, FastBoard> fastBoards = new HashMap<>();
+
+  private final Consumer<FastBoard> defaultBoardUpdate;
+
+  public FastBoardManager(Consumer<FastBoard> defaultBoardUpdate) {
+    this.defaultBoardUpdate = defaultBoardUpdate;
+  }
+
+  public final void add(Player player) {
+    val board = new FastBoard(player);
+    defaultBoardUpdate.accept(board);
+    this.fastBoards.put(player.getUniqueId(), board);
+  }
+
+  public final void addAll(Collection<Player> c) {
+    c.forEach(player -> add(player));
+  }
+
+  public final void addAll(Player... players) {
+    for (Player player : players) {
+      add(player);
+    }
+  }
+
+  public final void forEach(Consumer<FastBoard> consumer) {
+    this.fastBoards.values().forEach(consumer);
+  }
+
+  public final void remove(UUID uuid) {
+    val board = fastBoards.remove(uuid);
+    if (board != null) {
+      board.delete();
+    }
+  }
+
+  public final void clear() {
+    this.fastBoards.values().forEach(FastBoard::delete);
+    this.fastBoards.clear();
+  }
+
+}
