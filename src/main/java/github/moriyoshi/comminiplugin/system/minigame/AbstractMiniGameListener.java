f2a05e12523b3c70c2eaacba558b589a1a8d560e
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-06-09 16:17:54 +0900

    feat(minigame): Set blockBreak and blockPlace to Cancel by default. :sparkles:

--- src/main/java/github/moriyoshi/comminiplugin/system/minigame/AbstractMiniGameListener.java
+++ src/main/java/github/moriyoshi/comminiplugin/system/minigame/AbstractMiniGameListener.java
@@ -1,6 +1,8 @@
 package github.moriyoshi.comminiplugin.system.minigame;
 
 import github.moriyoshi.comminiplugin.system.InterfaceGameListener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public interface AbstractMiniGameListener<T extends AbstractMiniGame>
@@ -9,5 +11,15 @@ public interface AbstractMiniGameListener<T extends AbstractMiniGame>
   @Override
   default void quit(final PlayerQuitEvent e) {
     getMiniGame().leavePlayer(e.getPlayer());
+  }
+
+  @Override
+  default void blockBreak(BlockBreakEvent e) {
+    e.setCancelled(true);
+  }
+
+  @Override
+  default void blockPlace(BlockPlaceEvent e) {
+    e.setCancelled(true);
   }
 }
